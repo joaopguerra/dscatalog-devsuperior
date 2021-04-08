9cb5807693b7701bb25cea8a555d3be62b05397e
Joao Guerra <joaopaulomg_@hotmail.com>
2021-04-08 18:31:58 -0300

    Query: Products with categories

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -33,8 +33,11 @@ public class ProductService {
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest) {
         List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
-        Page<Product> list = productRepository.find(categories, name, pageRequest);
-        return list.map(x -> new ProductDTO(x));
+        Page<Product> page = productRepository.find(categories, name, pageRequest);
+
+        //getContent convert page para list
+        productRepository.findProductsWithCatefories(page.getContent());
+        return page.map(x -> new ProductDTO(x, x.getCategories()));
 
     }
 
